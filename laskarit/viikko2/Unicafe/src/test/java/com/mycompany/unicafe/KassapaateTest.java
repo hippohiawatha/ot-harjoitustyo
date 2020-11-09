43469a9a6b1bb70acc7a4aa794eb0d82377542fa
Leo Tavaila <leo.tavaila@helsinki.fi>
2020-11-09 13:57:45 +0200

    getting there

---
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.unicafe;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KassapaateTest {
+
+  Kassapaate kassa;
+
+  
+
+
+}
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,33 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti!=null);
     }
+
+    @Test
+    public void saldoAlussaOikein(){
+      assertTrue(kortti.saldo() == 10);
+    }
+
+    @Test
+    public void toStringOikein(){
+      assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void saldoKasvaaOikein(){
+      kortti.lataaRahaa(5);
+      assertTrue(kortti.saldo() == 15);
+    }
+
+    @Test
+    public void rahanOttaminenToimiiJosTarpeeksi(){
+      assertTrue(kortti.otaRahaa(9));
+    }
+
+    @Test
+    public void rahanOttaminenToimiiJosEiTarpeeksi(){
+      assertFalse(kortti.otaRahaa(11));
+    }
+
 }
